2c49dc527a47b79c5f130d16646b053c68588cff
MichaelBraun1994 <michael.braun1994@gmail.com>
2020-07-10 22:38:24 +0200

    Replaced MPI_Scatter with MPI_Send/MPI_Recv. Added Residium calculation and gathering with MPI_Reduce.

--- src/main.c
+++ src/main.c
@@ -5,61 +5,79 @@
 
 #include "matrix_handling.h"
 
-void sendBorders(float *dataPartition, int partitionSize, int N, int rank, int numberOfWorkers)
-{
-    MPI_Request req;
+#define ERROR_FUNCTION_CASE_1 1
+#define ERROR_FUNCTION_CASE_2 2
 
+void sendBorders(float* upperBorder, float* lowerBorder, int N, int rank, int numberOfWorkers)
+{
+    MPI_Request req; 
     if(rank == 1)
     {
-        MPI_Isend(&dataPartition[partitionSize * N], N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(lowerBorder, N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, &req);
     }
-    else if(rank == (numberOfWorkers - 1))
+    else if(rank == numberOfWorkers)
     {
-        MPI_Isend(&dataPartition[N], N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(upperBorder, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, &req);
     }
     else
     {
-        MPI_Isend(&dataPartition[partitionSize * N], N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, &req);
-        MPI_Isend(&dataPartition[N], N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(lowerBorder, N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(upperBorder, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, &req);
     }
 }
 
-void recvBorders(float *dataPartition, int partitionSize, int N, int rank, int numberOfWorkers, int initial)
+void recvBorders(float* upperBorder, float* lowerBorder, int N, int rank, int numberOfWorkers)
 {
     if(rank == 1)
     {
-        if(initial)
-        {
-            MPI_Recv(dataPartition, N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        MPI_Recv(&dataPartition[(partitionSize + 1) * N], N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(lowerBorder, N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    else if(rank == (numberOfWorkers - 1))
+    else if(rank == numberOfWorkers)
     {
-        MPI_Recv(dataPartition, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if(initial)
-        {
-            MPI_Recv(&dataPartition[(partitionSize + 1) * N], N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+        MPI_Recv(upperBorder, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     else
     {
-        MPI_Recv(dataPartition, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&dataPartition[(partitionSize + 1) * N], N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(lowerBorder, N, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(upperBorder, N, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+}
+
+void distributeData(float *dataField, int N, int numberOfWorkers, int rowsPerWorker)
+{
+    int sendIndex = 1; 
+
+    for(int i = 0; i < numberOfWorkers; ++i)
+    {
+        MPI_Send(&dataField[i * rowsPerWorker * N], (rowsPerWorker + 2) * N, MPI_FLOAT, sendIndex, 0, MPI_COMM_WORLD);
+        sendIndex++;
+    }
+}
+
+void collectData(float *dataField, int N, int numberOfWorkers, int rowsPerWorker)
+{
+    int recvIndex = 1;
+
+    for(int i = 0; i < numberOfWorkers; ++i)
+    {
+        MPI_Recv(&dataField[(i + 1) * N], (rowsPerWorker * N), MPI_FLOAT, recvIndex, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        recvIndex++;
     }
 }
 
 int main(int argc, char **argv)
 {
-    int rank = 0, workerCount = 0;
+    int rank = 0, numberOfWorkers = 0;
     int N = 10 + 2;
+    float residiumLimit = 0.0001;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &workerCount);
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfWorkers);
 
     int root = 0;
-    int partitionRowCount = ((N - 2) / (workerCount - 1));
+    numberOfWorkers -= 1;
+    int rowsPerWorker = ((N - 2) / numberOfWorkers);
 
     if (rank == root)
     {
@@ -67,21 +85,31 @@ int main(int argc, char **argv)
         float *dataField = createField(N);
         initDataField(dataField, N, 1.0f, 0.0f, 0.0f, 1.0f);
 
-        printf("Initial datafield.\n Rank %d, NodeCount: %d PartitionRowCount %d\n", rank, workerCount, partitionRowCount);
+        printf("Initial datafield.\n Rank %d, NodeCount: %d PartitionRowCount %d\n", rank, numberOfWorkers, rowsPerWorker);
         printDataField(dataField, N);
 
         /* scatter data */
-        MPI_Scatter(&dataField[N], partitionRowCount * N, MPI_FLOAT, &dataField[N], (N - 2) * N, MPI_FLOAT, root, MPI_COMM_WORLD);
-
-        /* send upper and lower matrix border */
-        MPI_Send(dataField, N, MPI_FLOAT, root + 1, 0, MPI_COMM_WORLD);
-        MPI_Send(&dataField[(N-1) * N], N, MPI_FLOAT, workerCount - 1, 0, MPI_COMM_WORLD);
+        distributeData(dataField, N, numberOfWorkers, rowsPerWorker);
 
         /* process */
+        float globalMaxResidium = 0.0f;
+        unsigned char continueExecution = 0x1;
+        {
+            //while(globalMaxResidium > residiumLimit)
+            for(int i = 0; i < 4; ++i)
+            {
+                MPI_Bcast(&continueExecution, 1, MPI_CHAR, root, MPI_COMM_WORLD);
+                MPI_Reduce(MPI_IN_PLACE, &globalMaxResidium, 1, MPI_FLOAT, MPI_MAX, root, MPI_COMM_WORLD);
+                printf("Maxresidium %f\n", globalMaxResidium);
+            }
+        }
 
+        /* stop iterating */
+        continueExecution = 0x0;
+        MPI_Bcast(&continueExecution, 1, MPI_CHAR, root, MPI_COMM_WORLD);
 
-        /* gather data */
-        MPI_Gather(MPI_IN_PLACE, 1, MPI_FLOAT, &dataField[N], partitionRowCount * N, MPI_FLOAT, root, MPI_COMM_WORLD);
+        /* collectData */
+        collectData(dataField, N, numberOfWorkers, rowsPerWorker);
 
         /* save results */
         printf("Data gathered:\n");
@@ -93,28 +121,39 @@ int main(int argc, char **argv)
     else
     {
         /* + 2 to store upper and lower border */
-        float *dataPartition = (float *) malloc(sizeof(float) * ((partitionRowCount + 2) * N));
-        float *workPartition = (float *) malloc(sizeof(float) * ((partitionRowCount + 2) * N));
-        MPI_Scatter(NULL, 0, MPI_FLOAT, &dataPartition[N], (partitionRowCount * N), MPI_FLOAT, root, MPI_COMM_WORLD);
+        int dataPartitionSize = (rowsPerWorker + 2) * N;
+        float *dataPartition = (float *) malloc(sizeof(float) * dataPartitionSize);
+        float *workPartition = (float *) malloc(sizeof(float) * dataPartitionSize);
 
-        sendBorders(dataPartition, partitionRowCount, N, rank, workerCount);
-        recvBorders(dataPartition, partitionRowCount, N, rank, workerCount, 1);
+        /* receive initial data */
+        MPI_Recv(dataPartition, dataPartitionSize, MPI_FLOAT, root, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        //while(0)
+        /* iterate over dataPartition until conductor signals end of execution */
+        unsigned char continueExecution = 1;
+        while(continueExecution)
         {
+            float localMaxResidium = 0.0f;
+            //float localMaxResidium = iterate(&dataPartition, &workPartition, rowsPerWorker, N, ERROR_FUNCTION_CASE_1);
 
-            /* calculate new entries */
-            float currentResidium = iterate(&dataPartition, &workPartition, N, partitionRowCount + 2, 1);
+            /* send residium to conductor */
+            MPI_Reduce(&localMaxResidium, NULL, 1, MPI_FLOAT, MPI_MAX, root, MPI_COMM_WORLD);
 
-            sendBorders(dataPartition, partitionRowCount, N, rank, workerCount);
-            recvBorders(dataPartition, partitionRowCount, N, rank, workerCount, 0);
+            /* check if another iteration is needed */
+            MPI_Bcast(&continueExecution, 1, MPI_CHAR, root, MPI_COMM_WORLD);
+
+            /* update neighbour nodes */
+            if(continueExecution)
+            {
+                sendBorders(dataPartition, &dataPartition[dataPartitionSize - N], N, rank, numberOfWorkers);
+                recvBorders(dataPartition, &dataPartition[dataPartitionSize -N], N, rank, numberOfWorkers);
+            }
         }
 
-        printf("Rank %d\n", rank);
-        printPartition(dataPartition, partitionRowCount + 2, N);
-        MPI_Gather(&dataPartition[N], partitionRowCount * N, MPI_FLOAT, NULL, 0, MPI_FLOAT, root, MPI_COMM_WORLD);
+        /* send result to conductor */
+        MPI_Send(&dataPartition[N], rowsPerWorker * N, MPI_FLOAT, root, 0, MPI_COMM_WORLD);
 
         free(dataPartition);
+        free(workPartition);
     }
     MPI_Finalize();
     return 0;
